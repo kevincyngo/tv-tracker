37cbd841253ec1b4f5f15fcab31ff1850aa0fde5
Kevin Ngo <ngokevin39@gmail.com>
2019-10-01 22:19:54 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App">
+    </HelloWorld>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 

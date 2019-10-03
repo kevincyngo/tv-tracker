8d0ee93ad8c5bb9813f6b8e04684283f7b3b5baf
Kevin Ngo <ngokevin39@gmail.com>
2019-10-02 23:13:41 -0400

    implemented sending API request to TVmaze

--- src/components/search.vue
+++ src/components/search.vue
@@ -4,7 +4,7 @@
       <img src="../assets/placeholder-homepage-logo.png">
     </div>
     <div class="search">
-      <input id="search-bar" type="text" placeholder="Search for tv show...">
+      <input id="search-bar" v-on:keyup.enter="searchForShows()" v-model="query" type="text" placeholder="Search for tv show...">
     </div>
     
   </div>
@@ -12,7 +12,25 @@
 
 <script>
 export default {
-    
+        data() {
+    return {
+      query: ""
+    };
+        },
+    methods: {
+        searchForShows() {
+            const Http = new XMLHttpRequest();
+            const url = "http://api.tvmaze.com/search/shows?q=" + this.query;
+            Http.open("GET", url);
+            Http.responseType = "json";
+            Http.send();
+
+            Http.onload = function() {
+                let responseObj = Http.response;
+                console.log(responseObj); // Hello, world!
+            };
+        }
+    }
 }
 </script>
 

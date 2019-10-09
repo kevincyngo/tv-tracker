c69b22b47779eafb73ba0d98dbd0848fccd806e0
Kevin Ngo <ngokevin39@gmail.com>
2019-10-09 00:25:46 -0400

    generate result list using v-for

--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -1,35 +1,49 @@
 <template>
-    <div class="results">
-        <!-- {{queryJSON}} -->
-        
-  <div v-for="item in responseTxt">{{item}}</div>
+  <div class="results">
+    <div v-for="item in responseTxt" class="search-results">
+      <div class="container">
+        <div class="img-container">
+          <img v-if="item.show.image != null" :src="item.show.image.medium" />
+          
+        </div>
+        <div class="details-container"> <h2>{{item.show.name}}</h2>
+            <span v-html="item.show.summary"></span>
+            {{item.show}}
+            <!-- show next episode (need to get another API call) -->
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'resultList',
-    props: [
-        'responseTxt'
-    ],
-    // computed: {
-    //     queryJSON: function() {
-    //         console.log(this.responseTxt);
-    //         if (this.responseTxt) {
-    //             // console.log(this.responseTxt[0].show._links.nextepisode.href)
-    //             return this.responseTxt;
-    //         }
-    //         return this.responseTxt;
-    //     }
-    // },
-
-  
-}
+  name: "resultList",
+  props: ["responseTxt"]
+};
 </script>
 
 <style scoped>
-* {
-    text-align:center;
-    margin:50px;
+.search-results {
+  text-align: left;
+  margin: 40px 20px 40px 20px;
+}
+
+.container {
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+}
+
+.img-container {
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+}
+
+.details-container {
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
 }
 </style>

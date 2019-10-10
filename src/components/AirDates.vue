b139b98b3a7e9528ac3b542b263f1880e234d803
Kevin Ngo <ngokevin39@gmail.com>
2019-10-10 16:07:00 -0400

    implemented previous and next episode air date and links to episode page

--- src/components/AirDates.vue
+++ src/components/AirDates.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="air-dates">
-      <p> {{previousAirDate}}</p>
-      <p> {{nextAirDate}}</p>
+    <span>
+      Previous air date: {{previousAirDate}} (<a :href="previousEpisodeURL" target="_blank">{{previousEpisodeName}}</a>)
+    </span>
+    <br />
+    <span>
+      Next air date: {{nextAirDate}}
+      <span v-if="nextEpisodeName != ''">
+        (<a :href="nextEpisodeURL" target="_blank">{{nextEpisodeName}}</a>)
+      </span>
+    </span>
   </div>
 </template>
 
@@ -9,60 +17,43 @@
 import EpisodesApi from "@/services/api/Episodes.js";
 
 export default {
-    name: 'airDates',
-    props:[
-        "episodeLinks"
-    ],
+  name: "airDates",
+  props: ["episodeLinks"],
 
-    data() {
-        return {
-            previousAirDate: "1",
-            nextAirDate: "2"
+  data() {
+    return {
+      previousAirDate: "",
+      previousEpisodeName: "",
+      previousEpisodeURL: "",
+      nextAirDate: "",
+      nextEpisodeName: "",
+      nextEpisodeURL: ""
+    };
+  },
+  async mounted() {
+    if (this.episodeLinks.previousepisode != null) {
+      await EpisodesApi.getEpisode(this.episodeLinks.previousepisode.href).then(
+        response => {
+          this.previousEpisodeName = response.name;
+          this.previousAirDate = response.airdate;
+          this.previousEpisodeURL = response.url;
         }
-    },
-    async mount() {
-        if (this.episodeLinks.previousepisode != null) {
-                await EpisodesApi.getEpisode(this.episodeLinks.previousepisode.href).then(
-                response => {
-                    this.previousAirDate = response.airdate;
-                });
-        }
-
-        if (this.episodeLinks.nextepisode != undefined) {
-                await EpisodesApi.getEpisode(this.episodeLinks.nextepisode.href).then(
-          response => {
-            this.nextAirDate = response.airdate;
-          }
-        );
-      } else {
-        this.nextAirDate = "No more episodes."
-      }
-
-    },
-
-    methods: {
-        async queryPrevEpisode() {
-            if (this.episodeLinks.previousepisode != null) {
-                await EpisodesApi.getEpisode(this.episodeLinks.previousepisode.href).then(
-                response => {
-                    this.previousAirDate = response.airdate;
-                });
-            }
-        },
-        async queryNextEpisode() {
-            if (this.episodeLinks.nextepisode != undefined) {
-                await EpisodesApi.getEpisode(this.episodeLinks.nextepisode.href).then(
-          response => {
-            this.nextAirDate = response.airdate;
-          }
-        );
-      } else {
-        this.nextAirDate = "No more episodes."
-      }
-        }
+      );
     }
 
-}
+    if (this.episodeLinks.nextepisode != undefined) {
+      await EpisodesApi.getEpisode(this.episodeLinks.nextepisode.href).then(
+        response => {
+          this.nextEpisodeName = response.name;
+          this.nextAirDate = response.airdate;
+          this.nextEpisodeURL = response.url;
+        }
+      );
+    } else {
+      this.nextAirDate = "No more episodes.";
+    }
+  }
+};
 </script>
 
 <style scoped>
--- src/components/ResultList.vue
+++ src/components/ResultList.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="results">
-    <div v-for="(show) in shows" class="search-results" :key="show.id">
+    <div v-for="(show) in shows" class="search-results" :key="show.show.id">
       <div class="container">
         <div class="img-container">
           <img v-if="show.show.image != null" :src="show.show.image.medium" />
+          <img v-else src="../assets/noimage.jpeg" />
         </div>
         <div class="details-container">
           <h3>{{show.show.name}}</h3>
           <span v-if="show.show.summary != null" v-html="show.show.summary"></span>
           <span v-else><p>Summary not available.</p></span>
           <span v-if="show.show.status != null"> Status: {{show.show.status}}</span>
-          <p v-if="show.show.status != 'Ended' && show.show.status != null">
+          <p v-if="show.show.status != null">
               <airDates :episodeLinks="show.show._links"></airDates>
-
           </p>
         </div>
       </div>
@@ -21,7 +21,6 @@
 </template>
 
 <script>
-import EpisodesApi from "@/services/api/Episodes.js";
 import airDates from "@/components/AirDates.vue";
 
 export default {
@@ -30,56 +29,12 @@ export default {
       airDates
   },
   props: ["shows"],
-  async mounted() {
-    for (var show of this.shows) {
-      if (show.show._links.previousepisode != null) {
-        EpisodesApi.getEpisode(show.show._links.previousepisode.href).then(
-          response => {
-            this.prevResponse.push(response.airdate);
-          }
-        );
-      }
-      if (show.show._links.nextepisode != undefined) {
-        await EpisodesApi.getEpisode(show.show._links.nextepisode.href).then(
-          response => {
-            this.nextEpisode.push(response.airdate);
-          }
-        );
-      } else {
-        this.nextEpisode.push("N/A");
-      }
-      
-    }
-  },
-
   data() {
     return {
       nextEpisode: [],
       prevResponse: []
     };
   },
-  methods: {
-    async loadData() {
-    for (var show of this.shows) {
-      if (show.show._links.previousepisode != null) {
-        await EpisodesApi.getEpisode(show.show._links.previousepisode.href).then(
-          response => {
-            this.prevResponse.push(response.airdate);
-          }
-        );
-      }
-      if (show.show._links.nextepisode != undefined) {
-        await EpisodesApi.getEpisode(show.show._links.nextepisode.href).then(
-          response => {
-            this.nextEpisode.push(response.airdate);
-          }
-        );
-      } else {
-        this.nextEpisode.push("N/A");
-      }
-    }
-  }
-  }
 };
 </script>
 
